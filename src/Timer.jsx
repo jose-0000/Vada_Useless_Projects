b98b330dc16b7bcac9f4517bae84d20e308dafb5
jose <jose.ann0000@gmail.com>
2025-08-01 23:19:58 +0530

    timer adjusted

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from 'react';
 function Timer({ initialSeconds = 30, onEnd }) {
   const [seconds, setSeconds] = useState(initialSeconds);
 
-  useEffect(() => {
-    if (seconds <= 0) {
-      if (onEnd) onEnd();
-      return;
-    }
-    const interval = setInterval(() => {
-      setSeconds((s) => s - 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [seconds, onEnd]);
+ useEffect(() => {
+  const interval = setInterval(() => {
+    setSeconds((s) => {
+      if (s <= 1) {
+        clearInterval(interval);
+        if (onEnd) onEnd();
+        return 0;
+      }
+      return s - 1;
+    });
+  }, 1000);
+  return () => clearInterval(interval);
+}, []);
 
   return (
     <div className="fixed top-4 left-4 text-2xl font-bold bg-blue-50 rounded-xl px-6 py-3 shadow text-center min-w-[120px]"
